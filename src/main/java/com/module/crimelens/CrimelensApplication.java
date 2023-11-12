64a7048d2f981435fab60342fafa8d65f2579320
vgnshiyer <vgnshiyer@gmail.com>
2023-11-12 12:41:58 -0700

    initial commit: added spring boot backend files

---
+++ src/main/java/com/module/crimelens/CrimelensApplication.java
@@ -0,0 +1,13 @@
+package com.module.crimelens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimelensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimelensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/module/crimelens/CrimelensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module.crimelens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimelensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
