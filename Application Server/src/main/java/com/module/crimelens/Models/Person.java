14ef2175cbc6a0cb04ef7a61b832ceebcc90e956
vgnshiyer <vgnshiyer@gmail.com>
2023-11-14 18:18:58 -0700

    fix: removed gradle temp build files

--- Application Server/src/main/java/com/module/crimelens/Models/Person.java
+++ Application Server/src/main/java/com/module/crimelens/Models/Person.java
@@ -1,5 +1,12 @@
 package com.module.crimelens.Models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Person {
     
     private Integer id;
