3d509f347fb9f0901208c8849414c5ef264564c3
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2023-11-23 18:41:30 -0700

    feat: added some dependencies

--- WebApp/src/App.jsx
+++ WebApp/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import HeaderComponent from './components/Header';
+import Profile from './components/Profile';
 
 function App() {
 
   return (
-    <div>
-      <h1>Crime Lens</h1>
+    <div className="App">
+      <Router>
+        <HeaderComponent />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
---
+++ WebApp/src/components/Profile.jsx
@@ -0,0 +1,6 @@
+
+export default function Profile() {
+    return (
+        <h1 style={{ marginTop: "200px" }}>Profile</h1>
+    )
+}
