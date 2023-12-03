044c0f44a535d05b31fc2eb20b7111fdeb7740bd
vgnshiyer <vgnshiyer@gmail.com>
2023-12-02 18:42:49 -0700

    final dataset generated

--- Data Integration/main.py
+++ Data Integration/main.py
@@ -1,34 +1,7 @@
-'''
-Author: Vignesh Iyer
-Date: 11/17/2023
-'''
-
-'''
-    CrimeEvent a owl:Class
-
-    CrimeEvent hasCrimeID Literal
-    CrimeEvent hasCrimeDate Literal
-    CrimeEvent hasClassification Literal
-
-    Victim a owl:Class
-    Victim hasAge Literal
-    Victim hasSex Literal
-    Victim hasRace Literal
-
-    Location a owl:Class
-
-    Location hasLattiude Decimal
-    Location hasLongitude Decimal
-    Location hasCity Literal
-    Location hasState Literal
-    Location hasStreet Literal
-'''
-
 from data_integration_engine import DataLoader
 
 INPUT_FILES_PATH = "raw_data/All_Crime_Data.csv"
 OUTPUT_FILE_PATH = 'fuseki/datasets/crime_lens.ttl'
-
 
 def main():
     data_loader = DataLoader(OUTPUT_FILE_PATH)
